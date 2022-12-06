7de9c0290d2f97458385983e43cf0da31891ca48
juan cu <Antony556_04@outlook.com>
2022-12-05 21:13:53 -0600

    cambios en el btn agregar equipo v2

--- resources/views/admin/ver_equipo.blade.php
+++ resources/views/admin/ver_equipo.blade.php
@@ -23,8 +23,8 @@
 
 @section('contenidoPrincipal')
     <div class="container">
-        <a href="{{ route('equipo.create') }}" class="ml-5 pl-5"><button type="button"
-                class="btn btn-info ml-5 mt-5 boton_redondo"><b>+</b> Agregar
+        <a href="{{ route('equipo.create') }}"><button type="button" class="btn btn-info ml-3 mt-5 boton_redondo"><b>+</b>
+                Agregar
                 Equipo</button></a>
     </div>
     <div class="container pl-5 mt-3">
